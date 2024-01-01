1e6ad617cc2b1defe7251221e3342eeabe017e79
BAD BABY <154504921+Badhacker98@users.noreply.github.com>
2024-01-01 10:31:29 +0530

    Update bot.py

--- AltBots/modules/bot.py
+++ AltBots/modules/bot.py
@@ -21,10 +21,10 @@ from datetime import datetime
 async def ping(e):
     if e.sender_id in SUDO_USERS:
         start = datetime.now()
-        altron = await e.reply(f"» __ᴀʟᴏɴᴇ__")
+        altron = await e.reply(f"» __ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ ᴛᴏ ғᴜᴄᴋ ᴛʜᴇsᴇ ʙɪᴛᴄʜᴇs__")
         end = datetime.now()
         mp = (end - start).microseconds / 1000
-        await altron.edit(f"__🤖 ᴘɪɴɢ__\n» `{mp} ᴍꜱ`")
+        await altron.edit(f"__😈 ριиg__\n» `{mp} ᴍꜱ`\n\n✯ 𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 ✯  : [🦋 𝐏ʙx 𝐁ᴏᴛ 𝐒ᴜᴘᴘᴏʀᴛ ❤️](https://t.me/ll_THE_BAD_BOT_ll)")
 
 
 @X1.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
