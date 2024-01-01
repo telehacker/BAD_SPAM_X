85e50a59472f181b38afaf24eeaf871959a59b0b
BAD BABY <154504921+Badhacker98@users.noreply.github.com>
2024-01-01 16:07:16 +0530

    Update help.py

--- AltBots/modules/help.py
+++ AltBots/modules/help.py
@@ -142,7 +142,7 @@ async def helpback(event):
             ]
           )
     else:
-        await event.answer("Make Your Own Altron Bots !! @ALONE_WAS_BOT", cache_time=0, alert=True)
+        await event.answer("Make Your Own Altron Bots !! @II_BAD_BBY_II", cache_time=0, alert=True)
 
 
 @X1.on(events.CallbackQuery(pattern=r"spam"))
@@ -161,7 +161,7 @@ async def help_spam(event):
               buttons=[[Button.inline("< Back", data="help_back"),],],
               ) 
     else:
-        await event.answer("Make Your Own Altron Bots !! @ALONE_WAS_BOT", cache_time=0, alert=True)
+        await event.answer("Make Your Own Altron Bots !! @II_BAD_BBY_II", cache_time=0, alert=True)
 
 
 @X1.on(events.CallbackQuery(pattern=r"raid"))
@@ -180,7 +180,7 @@ async def help_raid(event):
             buttons=[[Button.inline("< Back", data="help_back"),],],
           )
     else:
-        await event.answer("Make Your Own Altron Bots !! @ALONE_WAS_BOT", cache_time=0, alert=True)
+        await event.answer("Make Your Own Altron Bots !! @II_BAD_BBY_II", cache_time=0, alert=True)
 
 
 @X1.on(events.CallbackQuery(pattern=r"extra"))
@@ -199,4 +199,4 @@ async def help_extra(event):
             buttons=[[Button.inline("< Back", data="help_back"),],],
             )
     else:
-        await event.answer("Make Your Own Altron Bots !! @ALONE_WAS_BOT", cache_time=0, alert=True)
+        await event.answer("Make Your Own Altron Bots !! @II_BAD_BBY_II", cache_time=0, alert=True)
