76caa36eecfe008eba823d02a4cc440fc1d5cbad
BAD BABY <154504921+Badhacker98@users.noreply.github.com>
2024-01-01 16:05:22 +0530

    Update help.py

--- AltBots/modules/help.py
+++ AltBots/modules/help.py
@@ -60,7 +60,7 @@ extra_msg = f"""
   2) {hl}leave : Type in the Group bot will auto leave that group
 
 
-**© @ALONE_WAS_BOT**
+**© @II_BAD_BBY_II**
 """
 
                  
@@ -92,7 +92,7 @@ raid_msg = f"""
   2) {hl}craid <count> <reply to user>
 
 
-**© @ALONE_WAS_BOT**
+**© @II_BAD_BBY_II**
 """
 
 spam_msg = f"""
@@ -109,7 +109,7 @@ spam_msg = f"""
   1) {hl}hang <counter>
 
 
-** © @ALONE_WAS_BOT**
+** © @II_BAD_BBY_II**
 """                     
            
            
